d38d441f548c910dc8386fda76432f6e371fed62
Apisit Saithong <aps.apisit250@gmail.com>
2022-11-21 11:11:05 +0700

    Update 21-11-65

--- comsci/views.py
+++ comsci/views.py
@@ -1,52 +1,62 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import xlrd
 
 
-    
-
 # Create your views here.
+try:
+
+    information = pd.read_excel('database/excel/data.xlsx')
+    data = pd.DataFrame(information).set_index('ที่')
+except:
+    print("error")
 
 
 def homepage(request):
     # name = data['ชื่อ - สกุล']
     # fb = data['Facebook']
-    try :
-    
-        information = pd.read_excel('database/excel/data.xlsx')
-        data = pd.DataFrame(information).set_index('ที่')
-        pic = {
-        'datas': data,
-        'ID':data['รหัสนักศึกษา'],
-        'prefix':data['คำนำหน้า']
 
-        }
-    
-    except :
-        print("error")
-    
     pic = {
-        # 'data': data,
-        # 'ID':data['รหัสนักศึกษา'],
-        # 'prefix':data['คำนำหน้า']
-        
-        
+        'title': 'Home',
+        'datas': data,
+        'ID': data['รหัสนักศึกษา'],
+        'prefix': data['คำนำหน้า'],
+        'picture': ['pictures/form-icon.png', 'pictures/lib-logo.png'], 
+        'link': ['form', 'lib'], 
+        'name': ['Form', 'Library'], 
+        'text': ['งาน', 'sskru']
+
     }
-    
+    work = {'picture': ['pictures/form-icon.png', 'pictures/lib-logo.png'],
+            'link': ['form', 'lib'], 'name': ['Form', 'Library'], 'text': ['งาน', 'sskru']}
+
     # print(f'k {name.keys()} : v {name.values()}')
-   
+
     return render(request, 'home.html', pic)
 
 
 def form_page(request):
-    return render(request, 'work_pages/form.html')
+    data = {
+        "title": "Form"
+    }
+    return render(request, 'work_pages/form.html', data)
 
 
 def students(request):
-    return render(request, 'students.html')
+    main_data = {
+        "title": "Student",
+        "students":data.head()
+        
+    }
+    print(data)
+    return render(request, 'work_pages/students.html', main_data)
+
 
 def homepage_lib(request):
-    return render(request, 'work_pages/lib.html')
+    data = {
+        "title": "Library"
+    }
+    return render(request, 'work_pages/lib.html', data)
 
--- static/script/script.js
+++ static/script/script.js
@@ -3,29 +3,39 @@ function print(x) {
 }
 
 function calculate(x, y, z) {
-    if (y == '+'){
-        document.write(x+z)
+    if (y == '+') {
+        document.write(x + z)
     }
-    else if  (y == '-'){
-        document.write(x-z)
+    else if (y == '-') {
+        document.write(x - z)
     }
-    else if  (y == 'x' || y == '*'){
-        document.write(x*z)
+    else if (y == 'x' || y == '*') {
+        document.write(x * z)
     }
-    else if  (y == '/'){
-        document.write(x/z)
-    }
-}
-
-function multiply(x){
-    for (i = 1; i<= 12; i++){
-        document.write(`<h1 align="center" class="digital text-danger">${x} x ${i} = ${x*i}</h1>`)
+    else if (y == '/') {
+        document.write(x / z)
     }
 }
 
-$(function() {
-    $('#menu-title a').click(function(){
+function multiply(x) {
+    for (i = 1; i <= 12; i++) {
+        document.write(`<h1 align="center" class="digital text-danger">${x} x ${i} = ${x * i}</h1>`)
+    }
+}
+
+function title_manage(title) {
+    document.write(`<title>${title}</title>`);
+}
+
+$(function () {
+    $('#menu-title a').click(function () {
         $('#btn-title').text($(this).text());
     });
 });
 
+
+function data_set(arr){
+    for ( i = 0 ; i< arr.length; i++){
+        document.write(`<p>${arr[0][i]} == ${arr[1][i]}</p>`)
+    }
+}
